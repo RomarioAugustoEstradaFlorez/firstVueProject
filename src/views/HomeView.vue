6084f2e29c583fe3994f299e58b34625b12d7f50
Romario Estrada <romawild@gmail.com>
2023-04-20 22:38:34 -0500

    [ADD] The vue async component

---
+++ src/views/HomeView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" /> -->
+    <component v-bind:is="compToUse" v-bind="propsToUse"></component>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import { defineAsyncComponent } from "vue";
+
+const helloAsyncWorld = defineAsyncComponent(
+  () => import("../components/HelloWorld.vue")
+);
+
+@Options({
+  components: {
+    HelloWorld,
+    helloAsyncWorld,
+  },
+})
+export default class HomeView extends Vue {
+  public compToUse = "helloAsyncWorld";
+  public propsToUse = {
+    msg: "Lo que sea",
+  };
+}
+</script>
